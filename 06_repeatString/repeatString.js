f1ec37f1909bfb450390e95e0c29900db102d774
delirium02 <darknightbs@gmail.com>
2025-10-10 02:41:11 +1100

    successfully completed this exercise.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = (word, value) => {
+    if (value < 0) return "ERROR";
+    let string = "";
+    for(let i = 0; i < value; i++) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line

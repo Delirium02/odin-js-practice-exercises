b267e76dec8f1a7e993f95f2c9091deaa17c2de7
delirium02 <darknightbs@gmail.com>
2025-10-26 13:15:14 +1100

    solution for basic palindrome checker

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (isPalindrome) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const modifiedString = isPalindrome
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+    const reversedString = modifiedString.split('').reverse().join('');
+
+    return modifiedString === reversedString;
 };
 
 // Do not edit below this line
